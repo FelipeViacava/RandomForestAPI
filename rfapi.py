bd1cd5bcac21f104930f81703975740237365dc5
FelipeViacava <felipevf@al.insper.edu.br>
2022-11-05 02:43:22 -0300

    The model works :)

--- rfapi.py
+++ rfapi.py
@@ -9,29 +9,30 @@ with open('model.pickle', 'rb') as f:
 app = Flask(__name__)
 @app.route("/RandomForest", methods=['GET','POST'])
 def predict():
-    data = request.json
+    payload = request.json
 
-    data = data['data']
+    data = payload['data']
 
     df = pd.DataFrame()
     columns = list(data.keys())
 
     for col in columns:
         df[col] = [data[col]]
-        print(col)
     
-    #df['female'] = df['Sex'].apply(lambda sex: 1 if sex == 'female' else 0)
-    
-    #df['1Class'] = df['Pclass'].apply(lambda pclass: 1 if pclass==1 else 0)
-    #df['2Class'] = df['Pclass'].apply(lambda pclass: 1 if pclass==2 else 0)
-    #df = df.drop(['Sex','Pclass'],axis=1)
+    df['female'] = df['Sex'].apply(lambda sex: 1 if sex == 'female' else 0)
+    df['1Class'] = df['Pclass'].apply(lambda pclass: 1 if pclass==1 else 0)
+    df['2Class'] = df['Pclass'].apply(lambda pclass: 1 if pclass==2 else 0)
+    df = df.drop(['Sex','Pclass'],axis=1)
 
-    #x = df.values
-    #prediction = clf.predict(x)[0]
+    x = df.values
+    y = clf.predict(x)[0]
 
-    #return jsonify({'prediction': prediction})
+    if y == 1:
+        prediction = 'According to our model, you would survive the Titanic disaster!'
+    else:
+        prediction = 'According to our model, you would not survive the Titanic disaster.'
 
-    return jsonify({'True':True})
+    return jsonify({'Prediction': prediction})
 
 if __name__ == '__main__':
     app.run(debug=True)
